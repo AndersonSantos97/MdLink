d3f837fefafc2745cc5ad2df452b6aaa58f851f2
Anderson Santos <alopezsantos0@gmail.com>
2024-06-26 10:35:40 -0600

    Uppercase a input

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -34,7 +34,7 @@
             <div>
               <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Usuario</label>
               <div class=" text-center mt-2">
-                <input name="username" id="email"   required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                <input name="username" id="email" style="text-transform:uppercase;"  required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
               </div>
             </div>
       
