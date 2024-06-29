79ac1cab90fb002f2a066afb2183296c5dc914f1
RalzGT <Lpdefender1@gmail.com>
2024-06-29 12:03:45 -0600

    Creación del controlador de dispositivos

---
+++ app/Http/Controllers/DispositivoController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DispositivoController extends Controller
+{
+    // Mostrar dispositivos
+    public function index()
+    {
+        try {
+            $dispositivo = Dispositivo::all();
+            return view('dispositivo.index', compact('dispositivo'));
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+    //Mostrar un dispositivo especifico
+    public function show($id)
+    {
+        try {
+            $dispositivo = Dispositivo::findOrFail($id);
+            return view('dispositivo.show', compact('dispositivo'));
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    //muestra formulario para crear
+    public function create()
+    {
+        return view('dispositivo.create');
+    }
+    // Crear un departamento
+    public function store(Request $request)
+    {
+        try {
+            
+            return redirect()->route('dispositivo.index')->with('success', 'Dispositivo creado correctamente');
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+// 
+    public function edit($id)
+    {
+        try {
+            $dispositivo = Dispositivo::findOrFail($id);
+            return view('dispositivo.edit', compact('dispositivo'));
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+// Actualizar - editar dispositivo
+    public function update(Request $request, $id)
+    {
+        try {
+            
+            return redirect()->route('dispositivo.index')->with('success', 'Dispositivo actualizado correctamente');
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+    // eliminar dispositivo
+    public function destroy($id)
+    {
+        try {
+            $dispositivo = Dispositivo::findOrFail($id);
+            $dispositivo->delete();
+            return redirect()->route('dispositivo.index')->with('success', 'Dispositivo eliminado correctamente');
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}
