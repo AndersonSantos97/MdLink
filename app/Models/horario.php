c9505291e89fb9ff287daa0430a4d74a77b5d097
Silcaval <cvalderramos@gmail.com>
2024-07-01 20:08:17 -0600

    change in horario's model

--- app/Models/horario.php
+++ app/Models/horario.php
@@ -5,8 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class horario extends Model
+class Horario extends Model
 {
-    //use HasFactory;
-    protected $table = "horario";
+    use HasFactory;
+
+    protected $fillable = ['descripcion', 'hora_inicio', 'hora_fin'];
+
+    public function empleados()
+    {
+        return $this->hasMany(Empleado::class);
+    }
 }
