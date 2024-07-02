c31593303476cf2e7771ddf37d51332f97296f8f
boryspine <119823559+boryspine@users.noreply.github.com>
2024-07-01 20:38:12 -0600

    Update Usuarios.blade.php
    agregando boton de regresar

--- resources/views/Usuarios.blade.php
+++ resources/views/Usuarios.blade.php
@@ -67,6 +67,9 @@
             <button type="button" onclick="deleteUser('delete')" class="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium text-white duration-200 bg-gray-900 md:w-auto rounded-xl hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-black" aria-label="Primary action">
               Eliminar
             </button>
+              <button class="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium text-white duration-200 bg-gray-900 md:w-auto rounded-xl hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-black" aria-label="Primary action">
+              Regresar
+            </button>
             <button type="button" onclick="logOut('logout')" class="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium duration-200 bg-gray-100 md:w-auto rounded-xl hover:bg-gray-200 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" aria-label="Secondary action">
               Salir
             </button>
