144b1020813292feeac3ad52ff6d3c2d6118e629
boryspine <119823559+boryspine@users.noreply.github.com>
2024-07-01 19:17:09 -0600

    Update Menuvisor.blade.php
    + icon

--- resources/views/Menuvisor.blade.php
+++ resources/views/Menuvisor.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-
+    <link rel="icon" href="ManageUsers.png" type="">
     <title>MenuVisor</title>
 </head>
 <body>
