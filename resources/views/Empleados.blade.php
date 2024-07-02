9e1112cef89f58498d9c3fd52a1f1269b4cb2832
boryspine <119823559+boryspine@users.noreply.github.com>
2024-07-01 19:15:32 -0600

    Update Empleados.blade.php
    + icon

--- resources/views/Empleados.blade.php
+++ resources/views/Empleados.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Gestión de Empleados</title>
+  <link rel="icon" href="ManageUsers.png" type="">
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body >
